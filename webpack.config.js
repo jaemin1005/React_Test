9c242f7f5d11d432de4dfc289e975eed16248bd6
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-08 12:50:59 +0900

    Fix | webpack.config.js : babel-loader, test 정규식 변경

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         use: 'babel-loader',
         exclude: /node_modules/,
       },
