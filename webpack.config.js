8eca06121f567319aee5b243067461f167212149
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-08 11:40:05 +0900

    Feat | webpack.config.js : ts-loader 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/app.ts',
+  entry: './src/app.ts',
   output: {
     path: path.resolve(__dirname, 'webpack'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
 };
