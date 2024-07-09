8fb781f719e387434fc7b27387c4ee23d4eb3620
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-09 12:31:19 +0900

    Feat | app.tsx : UlLi 컴포넌트를 추가하였다

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "../public/css/test.css"
-import { minam } from "../static/basic-data"
+import { minam, listName } from "../static/basic-data"
 import Minam from './minam'
+import ULLi from './UlLi'
 
 const App:React.FC = () => {
   return (
     <div>
       <Minam textNode={minam}/>
       <h1>Hello world</h1>
+      <ULLi item={listName}/>
     </div>
   )
 }
